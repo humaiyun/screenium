ce3745e72938a03e12ee35878a33cb44c4f2d06c
Humaiyun Uddin <uddin513@msn.com>
2022-12-04 11:51:47 -0500

    Create Explore.js

---
+++ client/src/components/Explore/Explore.js
@@ -0,0 +1,13 @@
+import { Container, Typography } from '@mui/material'
+import React from 'react'
+
+const Explore = () => {
+  return (
+    <Container maxWidth="lg">
+      <Typography variant="h1">Explore</Typography>
+        
+    </Container>
+  )
+}
+
+export default Explore
