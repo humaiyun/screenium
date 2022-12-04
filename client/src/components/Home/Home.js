b5b99c09b1732c55029d8c849e9c8c0b6c2d3ea8
Humaiyun Uddin <uddin513@msn.com>
2022-12-04 12:43:46 -0500

    Added lorem ipsum for testing

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -4,8 +4,13 @@ import React from 'react'
 const Home = () => {
   return (
     <Container maxWidth="lg">
-      <Typography variant="h1">Home</Typography>
-        
+      <Typography variant="h1" gutterBottom>Home</Typography>
+      <Typography variant="body1" sx={{ paddingBottom: 250}} >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pellentesque at arcu quis dignissim. Vivamus sit amet nisi vehicula, ultricies orci at, lacinia turpis. Cras gravida, mi in posuere bibendum, nisi augue dapibus sem, sed pharetra purus enim sit amet risus. Cras eget nunc ornare, pretium turpis a, ullamcorper mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi faucibus mollis sem et pharetra. Integer vitae volutpat orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc efficitur in quam eu facilisis. Integer aliquet, nibh eget consectetur ultrices, arcu nisl congue est, a lobortis lacus risus eu nisl. Aliquam varius tempus feugiat. Nulla mollis eros eu mauris eleifend mattis. Nullam et semper purus.
+      </Typography>
+      <Typography variant="body1" sx={{ paddingBottom: 250}} >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pellentesque at arcu quis dignissim. Vivamus sit amet nisi vehicula, ultricies orci at, lacinia turpis. Cras gravida, mi in posuere bibendum, nisi augue dapibus sem, sed pharetra purus enim sit amet risus. Cras eget nunc ornare, pretium turpis a, ullamcorper mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi faucibus mollis sem et pharetra. Integer vitae volutpat orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc efficitur in quam eu facilisis. Integer aliquet, nibh eget consectetur ultrices, arcu nisl congue est, a lobortis lacus risus eu nisl. Aliquam varius tempus feugiat. Nulla mollis eros eu mauris eleifend mattis. Nullam et semper purus.
+      </Typography>
     </Container>
   )
 }
