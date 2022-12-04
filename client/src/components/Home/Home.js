041c1f746714a826c9a72c65142aef6e77c386a8
Humaiyun Uddin <uddin513@msn.com>
2022-12-04 11:51:50 -0500

    Create Home.js

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import { Container, Typography } from '@mui/material'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <Container maxWidth="lg">
+      <Typography variant="h1">Home</Typography>
+        
+    </Container>
+  )
+}
+
+export default Home
