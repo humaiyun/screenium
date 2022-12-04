fb8f732d70274cd9e26475bc2099d45cf4e2f37d
Humaiyun Uddin <uddin513@msn.com>
2022-12-04 12:43:36 -0500

    Move navbar outside main container

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import { Container } from '@mui/material';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Explore from './components/Explore/Explore';
 import Home from './components/Home/Home';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <BrowserRouter>
-      <Container maxWidth="xl">
-
+      <NavBar />
+      <Container maxWidth="xl" sx={{ border: "1px solid white"}}>
+        
         <Routes>
           <Route path="/" exact element={ <Home /> } />
           <Route path="/explore" exact element={ <Explore /> } />
