8a18cf65ca4312a70cf74a5df9c20d9cb9f99147
Humaiyun Uddin <uddin513@msn.com>
2022-12-04 11:51:40 -0500

    Added routes to home and explore

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-
+import { Container } from '@mui/material';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Explore from './components/Explore/Explore';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <Container maxWidth="xl">
 
-    </div>
+        <Routes>
+          <Route path="/" exact element={ <Home /> } />
+          <Route path="/explore" exact element={ <Explore /> } />
+        </Routes>
+        
+          
+      </Container>
+    </BrowserRouter>  
   );
 }
 
