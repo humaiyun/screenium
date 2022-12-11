82386afbe19d4fa38f3a78d191a5a21e19fe3988
Humaiyun Uddin <uddin513@msn.com>
2022-12-11 13:02:02 -0500

    renamed exports to be more descriptive

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const API = axios.create({ baseURL: process.env.REACT_APP_TMDB_BASE_URL })
 
 // Explore Page
-export const discoverPopularMovies = () => API.get(`/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`);
-export const trendingMovies = () => API.get(`/trending/movie/week?api_key=${process.env.REACT_APP_TMDB_API_KEY}`);
+export const getDiscoverPopularMovies = () => API.get(`/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`);
+export const getTrendingMovies = () => API.get(`/trending/movie/week?api_key=${process.env.REACT_APP_TMDB_API_KEY}`);
 
