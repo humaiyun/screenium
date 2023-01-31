7589e092cab71ee70a16034447328eaf9cf482ee
Humaiyun Uddin <uddin513@msn.com>
2023-01-31 11:08:22 -0500

    update server to connect to database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 import dotenv from "dotenv"
 dotenv.config();
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from 'cookie-parser';
+
+import connectDb from "./mongodb/connect.js";
 
 
 const app = express();
@@ -18,21 +19,21 @@ app.use(cors({
 }));
 app.use(cookieParser());
 
-
-// app.use("/user", userRoutes);
-// app.use("/employees", employeeRoutes);
+// app.use("/api/v1/user", userRoutes);
 
 app.get("/", (req, res) => {
-    res.json(
-        {
-            message: "Server is running"
-        }
-    );
+    res.json({ message: "Server is running" });
 });
 
-mongoose.connect(DB_STRING, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-})
-    .then(() => app.listen(PORT, () => console.log(`Connected to MongoDB\nListening on port ${PORT}`)))
-    .catch((err) => console.log(err.message));
+const startServer = async () => {
+    try {
+        connectDb(DB_STRING);
+        app.listen(PORT, () => console.log(`Server live - http://localhost:${PORT}`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+startServer();
+
+
