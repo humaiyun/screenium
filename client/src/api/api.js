35a46ee31faf3e2b6cc257a0eab6f782df8d9692
Humaiyun Uddin <uddin.humaiyun@gmail.com>
2023-03-09 17:30:50 -0500

    add search by movie, tv, and person api calls

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: process.env.REACT_APP_TMDB_BASE_URL });
 
-// Explore Page
-// export const getDiscoverPopularMovies = () => API.get(`/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`);
-
+// Explore Page and Search
 export const getTrendingMovies = () =>
   API.get(`/trending/movie/week?api_key=${process.env.REACT_APP_TMDB_API_KEY}`);
 export const getTrendingTV = () =>
@@ -13,6 +11,22 @@ export const getSearchQuery = (query) =>
   API.get(
     `/search/multi?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&query=${query}`
   );
+
+// Specific Search i.e., movies, tv, people
+export const getSearchMovies = (query) =>
+  API.get(
+    `/search/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&query=${query}`
+  );
+export const getSearchTV = (query) =>
+  API.get(
+    `/search/tv?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&query=${query}`
+  );
+export const getSearchPeople = (query) =>
+  API.get(
+    `/search/person?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&query=${query}`
+  );
+
+// Details for movie, tv, person
 export const getDetailsByID = (id) =>
   API.get(
     `/search/multi?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&query=${id}&page=1`
