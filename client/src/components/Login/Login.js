5a4cfc78814a95e548e50e9dcce758257e3b9a49
Shakil <shakil_miah@hotmail.com>
2023-03-09 15:16:28 -0500

    fixed code formatting to make it easier to read

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -9,22 +9,24 @@ const Login = () => {
 
   const formStyle = {borderRadius:10, padding: 20, height: '32vh', width: 480, margin: "40px auto", backgroundColor: "#303461"}
   return (
-    <Grid>
-      
+    <Grid>    
       <Paper elevation={10} style={formStyle}>
-      <Typography variant="h1"fontSize={32} sx={{
+      <Typography variant="h1"fontSize={32} 
+        sx={{
           color: 'white',
           fontWeight: "bold",
-          textAlign: "center"
-          
+          textAlign: "center"       
         }}>Login to your account</Typography>
-        <Typography variant="h1"fontSize={16} sx={{
-          color: 'white',
-          margin: "10px 0px",
-          textAlign: "center"
+
+        <Typography variant="h1"fontSize={16} 
+          sx={{
+            color: 'white',
+            margin: "10px 0px",
+            textAlign: "center"
         }}>In order to use the forums and personal list capabilities, you will need to login to your account.</Typography>
       
-      <TextField label="Username" variant="outlined" fullWidth required sx={{
+      <TextField label="Username" variant="outlined" fullWidth required 
+        sx={{
           bgcolor: '#000732',
           input: { color: 'white' },
           label: { color: 'white' },
@@ -37,13 +39,15 @@ const Login = () => {
           </InputAdornment>
         )}} 
         />
-      <TextField  label="Password" variant="outlined" margin="30px" type="password" fullWidth required sx={{
+      <TextField  label="Password" variant="outlined" margin="30px" type="password" fullWidth required 
+      sx={{
           bgcolor: '#000732',
           input: { color: 'white' },
           label: { color: 'white' },
           margin: "20px 0px",
           borderRadius: "10px"
-        }} InputProps={{startAdornment:(
+        }} 
+        InputProps={{startAdornment:(
           <InputAdornment position="start">
             <LockOutlinedIcon style={{ color: 'white' }}/>
           </InputAdornment>
@@ -51,7 +55,6 @@ const Login = () => {
         
         <Button
             className=" bg-main-background p-5 text-3xl font-semibold w-full rounded-full"
-
           >
             LOGIN
           </Button>
