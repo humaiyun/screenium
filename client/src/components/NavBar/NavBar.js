83bb42f6466c757ede02f3c5860573defd2acfe4
Humaiyun Uddin <uddin.humaiyun@gmail.com>
2023-03-11 14:47:49 -0500

    conditionally render navbar buttons and add logout button

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,17 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Typography, AppBar, Toolbar, Button, Box } from "@mui/material";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import logoImage from "../../assets/images/logo.png";
 
 const NavBar = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
   const [userType, setUserType] = useState("user");
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {}, []);
 
   useEffect(() => {
     setUserType(user?.userType);
+    if (user && user?.token) {
+      setIsLoggedIn(true);
+    }
   }, [location]);
+
+  const handleLogOut = () => {
+    localStorage.clear();
+    return location(0);
+  };
 
   return (
     <AppBar
@@ -36,6 +49,7 @@ const NavBar = () => {
         </Typography>
 
         <Box sx={{ display: { xs: "none", sm: "block" }, marginRight: "10%" }}>
+          {/* TODO: conditional rendering of these 3 buttons based on logged in & user role */}
           <Button component={Link} to="/" color="inherit">
             Home
           </Button>
@@ -48,37 +62,56 @@ const NavBar = () => {
           <Button component={Link} to="/profile" color="inherit">
             Profile
           </Button>
+          {isLoggedIn ? (
+            <>
+              <Button
+                component={Link}
+                to="/explore"
+                variant="contained"
+                sx={{
+                  backgroundColor: "#318986",
+                  marginRight: 1,
+                  borderRadius: "10px",
+                }}
+                onClick={handleLogOut}
+              >
+                LOG OUT
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button component={Link} to="/login" color="inherit">
+                Login
+              </Button>
+              <Button
+                component={Link}
+                to="/login?signup=true"
+                variant="contained"
+                sx={{
+                  backgroundColor: "#318986",
+                  marginRight: 1,
+                  borderRadius: "10px",
+                }}
+              >
+                Sign Up
+              </Button>
+            </>
+          )}
 
-          {/* TODO: conditional rendering of these 3 buttons based on logged in & user role */}
-          <Button component={Link} to="/login" color="inherit">
-            Login
-          </Button>
-
-          <Button
-            component={Link}
-            to="/login?signup=true"
-            variant="contained"
-            sx={{
-              backgroundColor: "#318986",
-              marginRight: 1,
-              borderRadius: "10px",
-            }}
-          >
-            Sign Up
-          </Button>
-
-          <Button
-            component={Link}
-            to="/admin"
-            variant="contained"
-            sx={{
-              backgroundColor: "#D8ECEB",
-              color: "black",
-              borderRadius: "10px",
-            }}
-          >
-            Admin Panel
-          </Button>
+          {userType === "admin" ? (
+            <Button
+              component={Link}
+              to="/admin"
+              variant="contained"
+              sx={{
+                backgroundColor: "#D8ECEB",
+                color: "black",
+                borderRadius: "10px",
+              }}
+            >
+              Admin Panel
+            </Button>
+          ) : null}
         </Box>
       </Toolbar>
     </AppBar>
