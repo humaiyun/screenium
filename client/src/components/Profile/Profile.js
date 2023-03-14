a4b0d7270fc77cd976e5e9d1767ed576b7b5ac49
Humaiyun Uddin <uddin.humaiyun@gmail.com>
2023-03-14 18:09:26 -0400

    complete most of profile page component

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,9 +1,89 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Profile = () => {
-  // TODO: Add conditional rendering if user is NOT logged in, then display an error component
-  return <Typography variant="h1">Profile</Typography>;
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+  const [userType, setUserType] = useState("user");
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setUserType(user?.userType);
+    if (user && user?.token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  return (
+    <div>
+      {!isLoggedIn ? (
+        <div className="bg-[#303461] p-16 my-80 rounded-3xl">
+          <h1 className="text-center text-5xl font-bold">
+            Please log in to view your profile
+          </h1>
+          <button
+            onClick={() => navigate("/login")}
+            className="my-16 w-full p-5 bg-main-primary rounded-3xl hover:scale-105 active:scale-95 transition duration-300"
+          >
+            Login
+          </button>
+        </div>
+      ) : (
+        <div className="p-10 bg-[#303461] m-16 mx-32 rounded-3xl justify-center items-center">
+          <div className="flex-row">
+            <h1 className="text-6xl font-bold mb-10 text-center">
+              {user?.existingUser?.username}
+            </h1>
+            <div>
+              <img
+                className="rounded-full pointer-events-none border-2 border-black mx-auto"
+                src={user?.existingUser?.profile_picture}
+                alt={`${user?.username}'s pfp`}
+                height="200"
+                width="200"
+              />
+            </div>
+            <div className="grid grid-cols-5 gap-10">
+              <div className="bg-main-background p-10 mt-8 rounded-3xl col-span-2">
+                <h2 className="text-4xl mb-3 underline underline-offset-4">
+                  About Me
+                </h2>
+                <h1 className="text-xl mb-8">
+                  <strong>Member Since:</strong>{" "}
+                  {new Date(
+                    `${user?.existingUser?.createdAt}`
+                  )?.toLocaleString()}
+                </h1>
+                <h1 className="text-xl mb-8">
+                  <strong>Name:</strong> {user?.existingUser?.firstName} &nbsp;
+                  {user?.existingUser?.lastName}
+                </h1>
+                <h1 className="text-xl mb-8">
+                  <strong>Email:</strong> {user?.existingUser?.email}
+                </h1>
+                <h1 className="text-xl mb-8">
+                  <strong>Gender:</strong> {user?.existingUser?.gender}
+                </h1>
+                <h1 className="mb-3">
+                  <strong className="text-xl">Biography:</strong>
+                  <span className="text-lg">
+                    &nbsp;{user?.existingUser?.biography}
+                  </span>
+                </h1>
+                <button className="w-full py-3 px-5 mt-5 bg-main-primary rounded-xl hover:scale-105 active:scale-95 transition duration-300 text-3xl font-bold">
+                  Edit Profile
+                </button>
+              </div>
+              <div className="bg-main-background p-10 mt-8 rounded-3xl col-span-3"></div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Profile;
