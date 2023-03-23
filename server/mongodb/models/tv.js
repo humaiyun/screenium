6f0531872a69b2cb2e27c59221b283ac98279eeb
Humaiyun Uddin <uddin.humaiyun@gmail.com>
2023-03-23 15:58:16 -0400

    complete tv discussion schema

---
+++ server/mongodb/models/tv.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const commentsSchema = new mongoose.Schema({
+  name: String,
+  comment_date: String,
+  comment: String,
+});
+
+const tv_discussion = new mongoose.Schema({
+  title: String,
+  submitted_date: String,
+  body: String,
+  comments: {
+    type: commentsSchema,
+  },
+});
+
+export default mongoose.model("tv_discussion", tv_discussion);
