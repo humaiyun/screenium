58dce5059ca634a3f95b1f94f5c0c609a6f7eede
Humaiyun Uddin <uddin.humaiyun@gmail.com>
2023-03-23 15:58:29 -0400

    add forums routes to server routes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 
 import connectDb from "./mongodb/connect.js";
 import userRoutes from "./routes/users.js";
+import forumRoutes from "./routes/forums.js";
 
 const app = express();
 const PORT = process.env.SERVER_PORT;
@@ -13,27 +14,30 @@ const DB_STRING = process.env.MONGODB_CONNECTION_STRING;
 
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-	origin: 'http://localhost:3000',
-	credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/forum", forumRoutes);
 
 app.get("/", (req, res) => {
-    res.json({ message: "Server is running" });
+  res.json({ message: "Server is running" });
 });
 
 const startServer = async () => {
-    try {
-        connectDb(DB_STRING);
-        app.listen(PORT, () => console.log(`Server live - http://localhost:${PORT}`));
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    connectDb(DB_STRING);
+    app.listen(PORT, () =>
+      console.log(`Server live - http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
-
-
