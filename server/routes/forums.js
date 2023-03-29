79053a30dd3865b0224f51e7f185079d19257ee1
Humaiyun Uddin <uddin.humaiyun@gmail.com>
2023-03-29 19:21:11 -0400

    add new routes

--- server/routes/forums.js
+++ server/routes/forums.js
@@ -1,6 +1,8 @@
 import express from "express";
 import {
+  getMovieDiscussionById,
   getMovieDiscussions,
+  getTvDiscussionById,
   getTvDiscussions,
 } from "../controllers/forums.js";
 import auth from "../middleware/auth.js";
@@ -8,6 +10,8 @@ import auth from "../middleware/auth.js";
 const router = express.Router();
 
 router.get("/tv/discussion", getTvDiscussions);
+router.get("/tv/discussion/:id", getTvDiscussionById);
 router.get("/movie/discussion", getMovieDiscussions);
+router.get("/movie/discussion/:id", getMovieDiscussionById);
 
 export default router;
