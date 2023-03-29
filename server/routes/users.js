d9001f8eb452a76496dd06996fbde4c9dc4a33b1
Humaiyun Uddin <uddin.humaiyun@gmail.com>
2023-03-29 17:30:26 -0400

    new route complete

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signin, signup } from "../controllers/users.js";
+import { signin, signup, getUsers } from "../controllers/users.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.get("/", getUsers);
 
 export default router;
