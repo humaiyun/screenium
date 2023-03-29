4778c423b82ec1ce213f55816866212a79881d84
Humaiyun Uddin <uddin.humaiyun@gmail.com>
2023-03-29 19:20:55 -0400

    add routes to get by id

--- server/controllers/forums.js
+++ server/controllers/forums.js
@@ -12,3 +12,31 @@ export const getMovieDiscussions = async (req, res) => {
 
   return res.status(200).json({ posts });
 };
+
+export const getTvDiscussionById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await tv_discussion.findById(id);
+
+    return res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({
+      message: "could not retrieve post details",
+      error: error.message,
+    });
+  }
+};
+
+export const getMovieDiscussionById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await movie_discussion.findById(id);
+
+    return res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({
+      message: "could not retrieve post details",
+      error: error.message,
+    });
+  }
+};
